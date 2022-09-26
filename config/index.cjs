938fa865723f52a901819a6c6bf717e193ba792e
wangcai7p <114343316+wangcai7p@users.noreply.github.com>
2022-09-26 17:01:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osX6d6XfSf-qfhJIrak382badeI8',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'OVDxb94_r4Wb7y0_P-_QqgGcdaVapVZDZaMM3ie0sqs',
+      useTemplateId: 'aOC0P4H28P0o0uQUUGEcuNwuMGS4VSGJT_giwwZ5MGA',
       // 所在省份
       province: '广东',
       // 所在城市
