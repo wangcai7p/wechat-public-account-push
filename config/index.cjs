527c43bebf5cf17db5630f4a4381a9334706fbe4
wangcai7p <114343316+wangcai7p@users.noreply.github.com>
2022-09-27 09:47:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wxfc7221eb2da4e480',
+  APP_ID: 'wx25168ce0fcf7e247',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '1a65b23b60bc969a39582e130fb796ae',
+  APP_SECRET: '0b3851dcf26fc20c3b42ae42610c4546',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -65,7 +65,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'osX6d6cmQmgkA9i8Q0A5mmoh3z7w',
+      id: 'osX6d6XfSf-qfhJIrak382badeI8',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'arw7cVFBjRiht1k4kWlifwukkltWx_W11AwYwt-a6d8',
       // 所在省份
